97bd54101fc00c4a4494653a585a657878562803
kingrain94 <kingrain94@gmail.com>
2016-11-07 14:40:02 +0000

    user rate

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,6 +2,7 @@ class Hotel < ActiveRecord::Base
     belongs_to :address
     has_many :images, dependent: :destroy
     has_many :comments, dependent: :destroy
+    ratyrate_rateable "hotel"
     validates :name, presence: true, length: {maximum: 100}
     validates :introduction, presence: true, length: {maximum: 100}
     validates :countRoom, presence: true
